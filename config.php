bbbbbb2a99965b117f1fb6c42ad9059c8c915fd4
davehack12 <itzdave400@gmail.com>
2026-05-20 12:15:54 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
-$host     = "localhost";
-$username = "root";
-$password = "";          // XAMPP default has NO password — leave blank
-$dbname   = "scms_db";
+
+$host = "sql305.infinityfree.com";
+$username = "if0_41960465";
+$password = "AWcdJ2dufQsI9";
+$dbname = "if0_41960465_SCM";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
